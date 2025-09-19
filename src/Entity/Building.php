210c8dbc70c8c97787925ce1271d00c68c081bde
Herr-chirurg <panzaniguihard@gmail.com>
2025-09-19 13:28:40 +0000

    création relation building character

--- src/Entity/Building.php
+++ src/Entity/Building.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\BuildingRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: BuildingRepository::class)]
@@ -18,6 +20,17 @@ class Building
 
     #[ORM\Column(length: 255)]
     private ?string $type = null;
+
+    /**
+     * @var Collection<int, Character>
+     */
+    #[ORM\OneToMany(targetEntity: Character::class, mappedBy: 'buildings')]
+    private Collection $Owner;
+
+    public function __construct()
+    {
+        $this->Owner = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -44,6 +57,36 @@ class Building
     public function setType(string $type): static
     {
         $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Character>
+     */
+    public function getOwner(): Collection
+    {
+        return $this->Owner;
+    }
+
+    public function addOwner(Character $owner): static
+    {
+        if (!$this->Owner->contains($owner)) {
+            $this->Owner->add($owner);
+            $owner->setBuildings($this);
+        }
+
+        return $this;
+    }
+
+    public function removeOwner(Character $owner): static
+    {
+        if ($this->Owner->removeElement($owner)) {
+            // set the owning side to null (unless already changed)
+            if ($owner->getBuildings() === $this) {
+                $owner->setBuildings(null);
+            }
+        }
 
         return $this;
     }
--- src/Entity/Character.php
+++ src/Entity/Character.php
@@ -41,6 +41,9 @@ class Character
     #[ORM\Column]
     private ?int $end_activity = null;
 
+    #[ORM\ManyToOne(inversedBy: 'Owner')]
+    private ?Building $buildings = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -150,6 +153,18 @@ class Character
     public function setEndActivity(int $end_activity): static
     {
         $this->end_activity = $end_activity;
+
+        return $this;
+    }
+
+    public function getBuildings(): ?Building
+    {
+        return $this->buildings;
+    }
+
+    public function setBuildings(?Building $buildings): static
+    {
+        $this->buildings = $buildings;
 
         return $this;
     }
