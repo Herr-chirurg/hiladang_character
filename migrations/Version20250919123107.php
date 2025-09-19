58578fb023c53745ec7fe3238acfd7b60201014f
Herr-chirurg <panzaniguihard@gmail.com>
2025-09-19 12:37:29 +0000

    archivage des migrations doctrine

---
+++ migrations/Version20250919123107.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250919123107 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE activity (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, type VARCHAR(255) NOT NULL, date DATETIME NOT NULL, cost_gp NUMERIC(8, 2) DEFAULT NULL, cost_pr NUMERIC(10, 0) DEFAULT NULL, duration INTEGER DEFAULT NULL, description VARCHAR(1000) NOT NULL)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE activity');
+    }
+}
