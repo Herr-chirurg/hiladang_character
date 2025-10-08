0af4a5e3ff02eaf025480723b49cc630f1621eb7
Herr-chirurg <panzaniguihard@gmail.com>
2025-10-08 17:58:16 +0000

    ajout nb character à scenario

--- src/Entity/Scenario.php
+++ src/Entity/Scenario.php
@@ -55,6 +55,9 @@ class Scenario
     #[ORM\ManyToMany(targetEntity: Character::class, inversedBy: 'scenarios')]
     private Collection $characters;
 
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 0)]
+    private ?string $nb_character = null;
+
     public function __construct()
     {
         $this->tokens = new ArrayCollection();
@@ -224,6 +227,18 @@ class Scenario
     public function removeCharacter(Character $character): static
     {
         $this->characters->removeElement($character);
+
+        return $this;
+    }
+
+    public function getNbCharacter(): ?string
+    {
+        return $this->nb_character;
+    }
+
+    public function setNbCharacter(string $nb_character): static
+    {
+        $this->nb_character = $nb_character;
 
         return $this;
     }
