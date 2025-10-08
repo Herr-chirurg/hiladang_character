ea014dbbb549ae081c074c0d15decf3042f1897f
Herr-chirurg <panzaniguihard@gmail.com>
2025-10-08 18:41:03 +0000

    recréation de la bdd

--- migrations/Version20251008184025.php
+++ migrations/Version20251008184025.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20251007114927 extends AbstractMigration
+final class Version20251008184025 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -31,13 +31,17 @@ final class Version20251007114927 extends AbstractMigration
         $this->addSql('CREATE INDEX IDX_15F4125AB3F38855 ON building_base_building_base (building_base_target)');
         $this->addSql('CREATE TABLE character (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, owner_id INTEGER DEFAULT NULL, name VARCHAR(255) DEFAULT NULL, title VARCHAR(255) DEFAULT NULL, img BLOB DEFAULT NULL, level INTEGER DEFAULT NULL, xp_current NUMERIC(7, 0) DEFAULT NULL, xp_current_mj NUMERIC(7, 0) DEFAULT NULL, gp NUMERIC(8, 2) DEFAULT NULL, pr NUMERIC(10, 0) DEFAULT NULL, end_activity INTEGER DEFAULT NULL, CONSTRAINT FK_937AB0347E3C61F9 FOREIGN KEY (owner_id) REFERENCES user (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
         $this->addSql('CREATE INDEX IDX_937AB0347E3C61F9 ON character (owner_id)');
-        $this->addSql('CREATE TABLE location (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, price NUMERIC(10, 0) DEFAULT NULL)');
-        $this->addSql('CREATE TABLE scenario (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, game_master_id INTEGER DEFAULT NULL, name VARCHAR(255) NOT NULL, level NUMERIC(2, 0) NOT NULL, date DATETIME NOT NULL, description VARCHAR(1000) NOT NULL, img BLOB NOT NULL, post_link VARCHAR(255) NOT NULL, summary_link VARCHAR(255) NOT NULL, status VARCHAR(255) NOT NULL, CONSTRAINT FK_3E45C8D8C1151A13 FOREIGN KEY (game_master_id) REFERENCES user (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('CREATE TABLE location (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) DEFAULT NULL)');
+        $this->addSql('CREATE TABLE scenario (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, game_master_id INTEGER DEFAULT NULL, name VARCHAR(255) NOT NULL, level NUMERIC(2, 0) NOT NULL, date DATETIME NOT NULL, description VARCHAR(1000) NOT NULL, img BLOB NOT NULL, post_link VARCHAR(255) NOT NULL, summary_link VARCHAR(255) NOT NULL, status VARCHAR(255) NOT NULL, nb_character NUMERIC(10, 0) NOT NULL, CONSTRAINT FK_3E45C8D8C1151A13 FOREIGN KEY (game_master_id) REFERENCES user (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
         $this->addSql('CREATE INDEX IDX_3E45C8D8C1151A13 ON scenario (game_master_id)');
-        $this->addSql('CREATE TABLE token (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, scenario_id INTEGER DEFAULT NULL, character_id INTEGER DEFAULT NULL, name VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, usage_rate INTEGER NOT NULL, value INTEGER NOT NULL, value_pr INTEGER NOT NULL, date_of_reception DATETIME NOT NULL, CONSTRAINT FK_5F37A13BE04E49DF FOREIGN KEY (scenario_id) REFERENCES scenario (id) NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_5F37A13B1136BE75 FOREIGN KEY (character_id) REFERENCES character (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('CREATE TABLE scenario_character (scenario_id INTEGER NOT NULL, character_id INTEGER NOT NULL, PRIMARY KEY(scenario_id, character_id), CONSTRAINT FK_81E6D42E04E49DF FOREIGN KEY (scenario_id) REFERENCES scenario (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_81E6D421136BE75 FOREIGN KEY (character_id) REFERENCES character (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('CREATE INDEX IDX_81E6D42E04E49DF ON scenario_character (scenario_id)');
+        $this->addSql('CREATE INDEX IDX_81E6D421136BE75 ON scenario_character (character_id)');
+        $this->addSql('CREATE TABLE token (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, scenario_id INTEGER DEFAULT NULL, character_id INTEGER DEFAULT NULL, owner_user_id INTEGER DEFAULT NULL, name VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, usage_rate INTEGER NOT NULL, value INTEGER NOT NULL, value_pr INTEGER NOT NULL, date_of_reception DATETIME NOT NULL, CONSTRAINT FK_5F37A13BE04E49DF FOREIGN KEY (scenario_id) REFERENCES scenario (id) NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_5F37A13B1136BE75 FOREIGN KEY (character_id) REFERENCES character (id) NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_5F37A13B2B18554A FOREIGN KEY (owner_user_id) REFERENCES user (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
         $this->addSql('CREATE INDEX IDX_5F37A13BE04E49DF ON token (scenario_id)');
         $this->addSql('CREATE INDEX IDX_5F37A13B1136BE75 ON token (character_id)');
-        $this->addSql('CREATE TABLE transfer (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, initiator_id INTEGER NOT NULL, recipient_id INTEGER NOT NULL, gp NUMERIC(8, 2) NOT NULL, pr NUMERIC(10, 0) NOT NULL, extra_pr NUMERIC(10, 0) NOT NULL, CONSTRAINT FK_4034A3C07DB3B714 FOREIGN KEY (initiator_id) REFERENCES character (id) NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_4034A3C0E92F8F78 FOREIGN KEY (recipient_id) REFERENCES character (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('CREATE INDEX IDX_5F37A13B2B18554A ON token (owner_user_id)');
+        $this->addSql('CREATE TABLE transfer (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, initiator_id INTEGER NOT NULL, recipient_id INTEGER DEFAULT NULL, gp NUMERIC(8, 2) NOT NULL, pr NUMERIC(10, 0) NOT NULL, extra_pr NUMERIC(10, 0) NOT NULL, description VARCHAR(1000) NOT NULL, CONSTRAINT FK_4034A3C07DB3B714 FOREIGN KEY (initiator_id) REFERENCES character (id) NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_4034A3C0E92F8F78 FOREIGN KEY (recipient_id) REFERENCES character (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
         $this->addSql('CREATE INDEX IDX_4034A3C07DB3B714 ON transfer (initiator_id)');
         $this->addSql('CREATE INDEX IDX_4034A3C0E92F8F78 ON transfer (recipient_id)');
         $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username VARCHAR(255) NOT NULL, role VARCHAR(255) NOT NULL, nb_character INTEGER NOT NULL)');
@@ -60,6 +64,7 @@ final class Version20251007114927 extends AbstractMigration
         $this->addSql('DROP TABLE character');
         $this->addSql('DROP TABLE location');
         $this->addSql('DROP TABLE scenario');
+        $this->addSql('DROP TABLE scenario_character');
         $this->addSql('DROP TABLE token');
         $this->addSql('DROP TABLE transfer');
         $this->addSql('DROP TABLE user');
