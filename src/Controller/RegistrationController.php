5db0bdacd780ff51f7f61db09d802eb22b41cd4d
Herr-chirurg <panzaniguihard@gmail.com>
2025-10-10 05:10:33 +0000

    authentification auto

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,19 +4,29 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
+use App\Security\AppCustomAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
-    {
-        $user = new User();
+   public function register(
+    Request $request,
+    UserAuthenticatorInterface $userAuthenticator,
+    AppCustomAuthenticator $authenticator,
+    UserPasswordHasherInterface $userPasswordHasher, // Correction: on garde l'injection pour le hachage
+    EntityManagerInterface $entityManager // Correction: une seule injection d'EntityManager
+    ): Response {
+         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
@@ -30,9 +40,14 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-            // do anything else you need here, like send an email
+            // 2. AUTO-LOG IN THE USER (Méthode propre et moderne)
+            // L'authenticator gère la création du token, la session et la redirection.
+            return $userAuthenticator->authenticateUser(
+                $user,
+                $authenticator,
+                $request
+            );
 
-            return $this->redirectToRoute('app_user_index');
         }
 
         return $this->render('registration/register.html.twig', [
