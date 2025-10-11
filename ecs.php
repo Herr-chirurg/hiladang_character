76562c3a25a17021643f4eac469af6ed0f3d458e
Herr-chirurg <panzaniguihard@gmail.com>
2025-10-11 00:16:56 +0000

    config quality

---
+++ ecs.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\Operator\ConcatSpaceFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use PhpCsFixer\Fixer\Import\GlobalNamespaceImportFixer;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return ECSConfig::configure()
+    ->withPaths([
+        __DIR__ . '/assets',
+        __DIR__ . '/config',
+        __DIR__ . '/public',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withPreparedSets(true)
+    ->withSkip([
+        ConcatSpaceFixer::class
+    ]);
