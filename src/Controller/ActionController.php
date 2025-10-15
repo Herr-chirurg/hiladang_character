eca9ed14646621ff548e8eb7b9711d98dcc9818c
herr_chirurg <panzaniguihard@gmail.com>
2025-10-15 22:30:37 +0000

    création du partiel action

---
+++ src/Controller/ActionController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\EntityActionService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ActionController extends AbstractController
+{
+    #[Route('/action', name: 'app_action')]
+    public function index(EntityActionService $actionService, Object $object): Response
+    {
+        return $this->render('partials/_action.html.twig', [
+            'object' => (new \ReflectionClass($object))->getShortName(),
+            'actions' => $actionService->getActions($object),
+        ]);
+    }
+}
---
+++ src/Service/EntityActionService.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Character;
+use App\Entity\User;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class EntityActionService
+{
+    // Injectez ici le Router (pour générer les liens) et le Security (pour les droits)
+    private $router;
+    private $security;
+
+    public function __construct(UrlGeneratorInterface $router, Security $security)
+    {
+        $this->router = $router;
+        $this->security = $security;
+    }
+
+    public function getActions(Object $object): array
+    {
+        get_class($object);
+
+        switch ($object::class) {
+            case Character::class:
+                return $this->getCharacterActions($object);
+        }
+        return [];
+    }
+
+    public function getCharacterActions(Character $character): array {
+
+        $user = $this->security->getUser();
+
+        if ($user instanceof User) {
+            $owner = $user == $character->getOwner();
+        }
+
+        $array = [];
+        
+        array_push($array, [
+            'label' => 'Retour',
+            'icon' => 'fa-solid fa-arrow-left',
+            'url' => $this->router->generate('app_character_index')
+        ]);
+
+        array_push($array, [
+            'label' => 'Editer',
+            'icon' => 'fa-solid fa-lock',
+            'url' => $owner ? $this->router->generate('app_character_edit', ['id' => $character->getId()]) : ""
+        ]);
+        
+        array_push($array, [
+            'label' => 'Echanger',
+            'icon' => 'fa-solid fa-handshake-angle',
+            'url' => ""
+        ]);
+        
+        array_push($array, [
+            'label' => 'Supprimer',
+            'icon' => 'fa-solid fa-skull',
+            'url' => $owner ? $this->router->generate('app_character_delete', ['id' => $character->getId()]) : ""
+        ]);
+
+        return $array;
+    }
+}
