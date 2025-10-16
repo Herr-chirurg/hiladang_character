484b985da85cf10e7c2c4a4f755a4f86984d0ee5
herr_chirurg <panzaniguihard@gmail.com>
2025-10-16 14:33:11 +0000

    création d'un voter scenario

---
+++ src/Security/Voter/ScenarioVoter.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Security\Voter;
+
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+final class ScenarioVoter extends Voter
+{
+    public const EDIT = 'EDIT';
+    public const DELETE = 'DELETE';
+
+    protected function supports(string $attribute, mixed $subject): bool
+    {
+        // replace with your own logic
+        // https://symfony.com/doc/current/security/voters.html
+        return in_array($attribute, [self::EDIT, self::DELETE])
+            && $subject instanceof \App\Entity\Scenario;
+    }
+
+    protected function voteOnAttribute(string $attribute, mixed $scenario, TokenInterface $token): bool
+    {
+        $securityUser = $token->getUser();
+
+        if (!$securityUser instanceof UserInterface) {
+            return false;
+        }
+
+        switch ($attribute) {
+            case self::EDIT:
+                return $securityUser == $scenario->getGameMaster();
+
+            case self::DELETE:
+                return $securityUser == $scenario->getGameMaster();
+        }
+
+        return false;
+    }
+}
