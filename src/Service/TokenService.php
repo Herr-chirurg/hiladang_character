aa3251e5b033e927800014810da711c3b7b5f218
herr_chirurg <panzaniguihard@gmail.com>
2025-11-26 02:04:13 +0000

    fix typo, suppression des champs xp

---
+++ src/Service/TokenService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Scenario;
+use App\Entity\Token;
+
+class TokenService {
+
+	
+	public function __construct()
+    { 
+	
+	}
+
+    public function findConsumableTo
+
+}
