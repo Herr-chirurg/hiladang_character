51f3e4e3bc1b7bdbb08390c5e6a3e42116761adb
herr_chirurg <panzaniguihard@gmail.com>
2025-12-12 03:06:03 +0000

    Fix webhook absent du panel admin

--- src/Controller/Admin/CharacterCrudController.php
+++ src/Controller/Admin/CharacterCrudController.php
@@ -6,9 +6,12 @@ use App\Entity\Character;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Validator\Constraints\Date;
 
 class CharacterCrudController extends AbstractCrudController
 {
@@ -23,6 +26,15 @@ class CharacterCrudController extends AbstractCrudController
         yield TextField::new('title');
         yield AssociationField::new('owner')
             ->setCustomOption('field_label', 'username');
+        yield IntegerField::new('level');
+        yield IntegerField::new('xp_current');
+        yield IntegerField::new('xp_current_mj');
+        yield IntegerField::new('gp');
+        yield IntegerField::new('pr');
+        yield DateTimeField::new('end_activity');
+        yield TextField::new('webhook_link');
+        yield TextField::new('last_action');
+
     }
 
 }
