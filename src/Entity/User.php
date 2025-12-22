95d7fad3fb5d0e827c0db196de0f1dcc4ea86ebd
herr_chirurg <panzaniguihard@gmail.com>
2025-12-22 09:04:41 +0000

    Ajout api user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,33 +9,49 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Get;
 
-#[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_DISCORD_ID', fields: ['discord_id'])]
 #[UniqueEntity(fields: ['discord_id'], message: 'There is already an account with this discord_id')]
-class User implements UserInterface, PasswordAuthenticatedUserInterface
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+    #[ApiResource(
+        normalizationContext: ['groups' => ['owner:read']],
+        stateless: false,
+        operations: [
+            // Permet l'affichage de la collection (GET /clients)
+            new GetCollection(),
+            // Permet l'affichage d'un seul élément (GET /clients/{id})
+            new Get(),
+        ],
+    )]class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
+
     #[ORM\Column(length: 180)]
     private ?string $discord_id = null;
 
+    #[Groups(['owner:read'])]
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $username = null;
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $role = null;
 
+    #[Groups(['owner:read'])]
     #[ORM\Column]
     private ?int $nb_character_max = 1;
 
     /**
      * @var list<string> The user roles
      */
-    #[ORM\Column]
+    #[Groups(['owner:read'])]
     private array $roles = [];
 
     /**
